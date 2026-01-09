a7fb7c9a29458fbc80163fe80a032b93f59a187f
NADIM HOSSAIN <23-53906-3@student.aiub.edu>
2026-01-09 19:11:39 +0600

    Dashboard form design added

---
+++ Construction ERP  Management System/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Construction_ERP__Management_System
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
